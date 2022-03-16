88788fba81fc98630921722423d697dff07dd3be
Zahra <bghzahra@outlook.com>
2022-03-16 17:06:36 +0000

    added kanye west quotes API fetch in useEffect async function

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
 import { useEffect, useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+interface Quote {
+  quote:string;
 }
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  const [quote, setQuote] = useState<Quote |string>();
 
   useEffect(() => {
-    const fetchJoke = async () => {
+    const fetchQuote = async () => {
       const response = await fetch(
-        "https://jokestemp.neillbogie.repl.co/jokes/general/random"
+        "https://api.kanye.rest"
       );
-      const jsonBody: Joke[] = await response.json();
-      setJoke(jsonBody[0]);
+      const jsonBody: Quote = await response.json();
+      setQuote(jsonBody.quote);
     };
 
-    fetchJoke();
+    fetchQuote();
   }, []);
 
   // useEffect(() => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
+  //   fetch("https://Quotestemp.neillbogie.repl.co/Quotes/general/random")
   //     .then(response => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
+  //     .then((jsonBody: Quote[]) => setQuote(jsonBody[0]));
   // }, [])
 
   return (
     <>
-      <h1>Joke app</h1>
-      {joke && (
+      <h1>Kanye quote app</h1>
+      {console.log(quote)}
+      {quote && (
         // This is a conditional rendering strategy
         //  using 'short-circuiting': if the left-hand
         //  side of an && is false, then JavaScript
@@ -42,10 +40,7 @@ function App() {
         // Exploiting that feature to conditional render JSX!
         <>
           <p>
-            <b>{joke.setup}</b>
-          </p>
-          <p>
-            <i>{joke.punchline}</i>
+            <b>{quote}</b>
           </p>
         </>
       )}
